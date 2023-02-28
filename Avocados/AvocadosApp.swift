84d531b54b56dd4643451f95b00ac36360f7ee6a
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-02-27 21:15:10 -0300

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Orlando Moraes Martins on 27/02/23.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
