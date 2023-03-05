8d2920857b7ca10ff662ff8397d8a0ef56ec7be8
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-03-05 12:07:46 -0300

    Creating the design of the ripening view

---
+++ Avocados/View/RipeningView.swift
@@ -0,0 +1,119 @@
+//
+//  RipeningView.swift
+//  Avocados
+//
+//  Created by Orlando Moraes Martins on 05/03/23.
+//
+
+import SwiftUI
+
+struct RipeningView: View {
+    //MARK: - Properties
+    @State private var slideInAnimation = false
+    
+    //MARK: - Body
+    var body: some View {
+        VStack {
+            Image("avocado-ripening-1")
+                .resizable()
+                .frame(width: 100, height: 100, alignment: .center)
+                .clipShape(Circle())
+                .background(
+                    Circle()
+                        .fill(Color("ColorGreenLight"))
+                        .frame(width: 110, height: 110, alignment: .center)
+                )
+                .background(
+                    Circle()
+                        .fill(Color("ColorAppearenceAdaptative"))
+                        .frame(width: 120, height: 120, alignment: .center)
+                )
+                .zIndex(1)
+                .animation(Animation.easeInOut(duration: 1))
+                .offset(y: slideInAnimation ?  55 : -55)
+            
+            VStack(alignment: .center, spacing: 10) {
+                //Stage
+                VStack(alignment: .center, spacing: 0) {
+                    Text("1")
+                        .font(.system(.largeTitle, design: .serif))
+                        .fontWeight(.bold)
+                    
+                    Text("Stage")
+                        .font(.system(.body, design: .serif))
+                        .fontWeight(.heavy)
+                }
+                .foregroundColor(Color("ColorGreenMedium"))
+                .padding(.top, 65)
+                .frame(width: 180)
+                
+                
+                //Title
+                Text("Hard")
+                    .font(.system(.title, design: .serif))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("ColorGreenMedium"))
+                    .padding(.vertical, 12)
+                    .padding(.horizontal, 0)
+                    .frame(width: 220)
+                    .background(
+                        RoundedRectangle(cornerRadius: 12)
+                            .fill(LinearGradient(gradient: Gradient(colors: [.white, Color("ColorGreenLight")]), startPoint: .top, endPoint: .bottom))
+                            .shadow(color: Color("ColorBlackTransparentLight"), radius: 6, x: 0, y: 6)
+                    )
+                
+                //Description
+                Spacer()
+                
+                Text("Fresh of the tree, the avocado is very hard to give.")
+                    .foregroundColor(Color("ColorGreenDark"))
+                    .fontWeight(.bold)
+                    .lineLimit(nil)
+                
+                Spacer()
+                
+                //Ripeness
+                Text("5+ Days")
+                    .foregroundColor(.white)
+                    .font(.system(.callout, design: .serif))
+                    .fontWeight(.bold)
+                    .shadow(radius: 3)
+                    .padding(.vertical)
+                    .padding(.horizontal, 0)
+                    .frame(width: 185)
+                    .background(
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(LinearGradient(gradient: Gradient(colors: [Color("ColorGreenMedium"), Color("ColorGreenDark")]), startPoint: .top, endPoint: .bottom))
+                            .shadow(color: Color("ColorBackTransparentLight"), radius: 6, x: 0, y: 6)
+                    )
+                
+                //Instructions
+                Text("Hold avocados at room temperature until they are fully ripe")
+                    .font(.footnote)
+                    .foregroundColor(Color("ColorGreenLight"))
+                    .fontWeight(.bold)
+                    .lineLimit(3)
+                    .frame(width: 160)
+                
+                Spacer()
+            }
+            .zIndex(0)
+            .multilineTextAlignment(.center)
+            .padding(.horizontal)
+            .frame(width: 260, height: 485, alignment: .center)
+            .background(LinearGradient(gradient: Gradient(colors: [Color("ColorGreenLight"),  Color("ColorGreenMedium")]), startPoint: .top, endPoint: .bottom))
+            .cornerRadius(20)
+        }//: VSTACK
+        .edgesIgnoringSafeArea(.all)
+        .onAppear(perform: {
+            self.slideInAnimation.toggle()
+        })
+    }
+}
+
+//MARK: - Preview
+struct RipeningView_Previews: PreviewProvider {
+    static var previews: some View {
+        RipeningView()
+    }
+}
